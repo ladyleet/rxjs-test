2b714f34b111ce91cccb38c95c214486e3c6754a
Ben Lesh <ben@benlesh.com>
2017-03-10 15:23:18 -0800

    style(pun-service): format fixes

--- src/app/pun-service.service.ts
+++ src/app/pun-service.service.ts
@@ -2,21 +2,27 @@ import { Injectable } from '@angular/core';
 import { Observable, asap } from './app.rx';
 
 const keywords = {
-    'banana': [0, 13, 14, 15, 16],
-    'cheese': [3, 4, 5, 6, 7, 8, 12],
-    'bird': [3, 11, 12, 1],
-    'hotel': [4, 9, 10, 11],
-    'mexican': [6], 
-    'cow': [12]
-  };
+  'banana': [0, 13, 14, 15, 16],
+  'cheese': [3, 4, 5, 6, 7, 8, 12],
+  'bird': [3, 11, 12, 1],
+  'hotel': [4, 9, 10, 11],
+  'mexican': [6], 
+  'cow': [12]
+};
 
 
 const puns = {
-    '0': { pun: 'My wife has banana diet', answer: 'She hasnt lost weight, but you should see her climb trees now!'
+  '0': {
+    pun: 'My wife has banana diet',
+    answer: 'She hasnt lost weight, but you should see her climb trees now!'
   },
-    '1': { pun: 'Id make a pun about a chicken...', answer: 'But it would be fowl'
+  '1': { 
+    pun: 'Id make a pun about a chicken...', 
+    answer: 'But it would be fowl'
   },
-    '2': { pun: 'this is a pun about a pickle and a hat', answer: 'answer'
+  '2': {
+    pun: 'this is a pun about a pickle and a hat', 
+    answer: 'answer'
   }
 };
 
