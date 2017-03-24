588b1c3457c25d5efa19bf07a8ee5ac89922863a
Ben Lesh <ben@benlesh.com>
2017-03-23 23:12:45 -0700

    tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,10 @@ import { PunLookupComponent } from './pun-lookup/pun-lookup.component';
 import { SnapshotCameraComponent } from './snapshot-camera/snapshot-camera.component';
 
 // add RxJS operators
-import 'rxjs/add/observable/empty';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/observable/merge';
 
 @NgModule({
   declarations: [
