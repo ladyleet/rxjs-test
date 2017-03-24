09718c388f12b8be3e11248670f43bf3581d2c82
Ben Lesh <ben@benlesh.com>
2017-03-23 23:00:07 -0700

    back to basics

--- src/app/pun-lookup/pun-lookup.component.ts
+++ src/app/pun-lookup/pun-lookup.component.ts
@@ -2,53 +2,27 @@ import { Component, Inject } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { PunService } from '../pun-service.service';
-import { SpeechService } from '../speech.service';
-import { GoogleVisionService } from '../google-vision.service';
 
 @Component({
   selector: 'app-pun-lookup',
   templateUrl: './pun-lookup.component.html',
   styles: [],
   providers: [
-    PunService,
-    SpeechService,
-    GoogleVisionService
+    PunService
   ]
 })
 export class PunLookupComponent {
 
   keywordsInputChange$ = new Subject<string>();
 
-  listenClick$ = new Subject<void>();
-
-  snapshot$ = new Subject<{ dataURL: string }>();
-
-  googleVision$ = this.snapshot$
-    .map(e => {
-      const dataURLHeader = 'data:image/png;base64,';
-      const base64Image = e.dataURL.substr(dataURLHeader.length);
-      return base64Image;
-    })
-    .switchMap(base64Image => this.googleVision.annotateImage(base64Image));
-
-  spokenKeyword$ = this.listenClick$
-    .switchMap(() => this.speech.listen())
-
-  typedKeyword$ = this.keywordsInputChange$
-    .switchMap(text => this.puns.suggestKeywords(text));
-
-  keyword$ = Observable.merge(
-    this.typedKeyword$,
-    this.spokenKeyword$,
-    this.googleVision$
-  ).share();
+  keyword$ = this.keywordsInputChange$
+    .switchMap(text => this.puns.suggestKeywords(text))
+    .share();
 
   punsFound$ = this.keyword$
       .switchMap(keywords => this.puns.getPuns(keywords))
 
   constructor(
-    private puns: PunService, 
-    private speech: SpeechService,
-    private googleVision: GoogleVisionService
+    private puns: PunService
   ) {}
 }
